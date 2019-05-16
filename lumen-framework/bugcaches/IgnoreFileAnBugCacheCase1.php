a6304b0b105c385515fd0066d80b1e96e3d11964
graig-patrick <graig.patrick@gmail.com>
2019-05-16 17:03:13 +0800

    fixes for 1061776 (#17)
    Should display the commit sha for ticket 1061776
    
    - Unable to see the commit that did the bug-inducing-changes
    
    Ticket: 1061779
    Reviewer: Graig Patrick

--- lumen-framework/bugcaches/IgnoreFileAnBugCacheCase1.php
+++ lumen-framework/bugcaches/IgnoreFileAnBugCacheCase1.php
@@ -6,13 +6,14 @@ class mock
     public function method1UpdatedByPrerequisite2()
     {
         $variable = "string tests";
-
+        $newFile = 'file';
         return $variable;
     }
 
     public function method2()
     {
         $variable = "string tests";
+
         return $variable;
     }
 }
