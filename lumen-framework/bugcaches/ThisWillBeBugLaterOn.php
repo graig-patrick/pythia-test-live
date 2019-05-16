10307bdd1843a9960cffc6cb067f78ab46d7da99
graig-patrick <graig.patrick@gmail.com>
2019-05-16 16:26:20 +0800

    changes that produce bug (#14)
    Bug inducing changes
    
    - This commit will cause a bug in the future
    
    Ticket: 1061776
    Reviewer: Graig Patrick

--- lumen-framework/bugcaches/ThisWillBeBugLaterOn.php
+++ lumen-framework/bugcaches/ThisWillBeBugLaterOn.php
@@ -6,6 +6,16 @@ class ThisWillBeBugLaterOn
 
     public function dummyFunction()
     {
+        $varzero = '0';
+        $varone = '1';
+        $vartwo = '2';
+        $varthree = '3';
+
+        return $varone . $vartwo . $varthree;
+    }
+
+    public function newFunction()
+    {
         $varone = '1';
         $vartwo = '2';
 
