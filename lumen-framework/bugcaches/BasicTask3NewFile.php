ce19949ebb3a0de650a48a2d8da8d2d0429d397c
Graig Patrick <graig.patrick@gmail.com>
2019-05-16 16:39:26 +0800

    changes after bug reported

---
+++ lumen-framework/bugcaches/BasicTask3NewFile.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class BasicTask3NewFile
+{
+
+
+    /**
+     * BasicTask3NewFile constructor.
+     */
+    public function __construct() { }
+
+    public function dummyMethod()
+    {
+        return false;
+    }
+}
--- lumen-framework/bugcaches/ThisWillBeBugLaterOn.php
+++ lumen-framework/bugcaches/ThisWillBeBugLaterOn.php
@@ -4,11 +4,17 @@
 class ThisWillBeBugLaterOn
 {
 
+
+    /**
+     * ThisWillBeBugLaterOn constructor.
+     */
+    public function __construct() { }
+
     public function dummyFunction()
     {
         $varzero = '0';
-        $varone = '1';
-        $vartwo = '2';
+        $varone = '3';
+        $vartwo = '4';
         $varthree = '3';
 
         return $varone . $vartwo . $varthree;
