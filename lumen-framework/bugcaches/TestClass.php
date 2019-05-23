e4ac0fc2309a356890bead07f008a2410eca3527
Graig Patrick <graig.patrick@gmail.com>
2019-05-23 15:04:06 +0800

    new test

--- lumen-framework/bugcaches/TestClass.php
+++ lumen-framework/bugcaches/TestClass.php
@@ -10,6 +10,8 @@ class TestClass
         // do something
         // do something
         // do something again
+        // do something again
+        // do something again
         return false;
     }
 }
