8c34afbf64d4da672c6b2e234d0de0dcce984569
Graig Patrick <graig.patrick@gmail.com>
2019-05-27 17:11:46 +0800

    modified existing bugfix file

--- lumen-framework/bugcaches/NewBugFixFile.php
+++ lumen-framework/bugcaches/NewBugFixFile.php
@@ -8,4 +8,9 @@ class NewBugFixFile
      * NewBugFixFile constructor.
      */
     public function __construct() { }
+
+    public function getFixes()
+    {
+        // TODO something to do here
+    }
 }
