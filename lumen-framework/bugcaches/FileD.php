ead4fb896842c9b5580f5e4ea94c682bc644b7e6
graig-patrick <graig.patrick@gmail.com>
2019-06-24 17:44:18 +0800

    this is bug introducing commit (#53)
    This is a bug introducing commit
    
    - added 2 new files.
    
    Ticket: 1065153
    Reviewer: Graig Patrick

---
+++ lumen-framework/bugcaches/FileD.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class FileD
+{
+
+    /**
+     * FileD constructor.
+     */
+    public function __construct() { }
+
+    public function getMethod()
+    {
+        return false;
+    }
+}
