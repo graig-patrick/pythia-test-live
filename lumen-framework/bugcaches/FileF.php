e1c968c44f445abec6f7dc9301d40522df23c29a
graig-patrick <graig.patrick@gmail.com>
2019-06-26 16:03:34 +0800

    1065214 bug for previous task (#56)
    Fixing bug
    
    - do changes
    
    Ticket: 1065214
    Reviewer: Graig Patrick

--- lumen-framework/bugcaches/FileF.php
+++ lumen-framework/bugcaches/FileF.php
@@ -4,4 +4,13 @@
 class FileF
 {
 
+    /**
+     * FileF constructor.
+     */
+    public function __construct() { }
+
+    public function newMethodForTest()
+    {
+        return true;
+    }
 }
