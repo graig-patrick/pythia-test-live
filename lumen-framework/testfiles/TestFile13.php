6bf36a44cc987b1245838cffd0060446d60f353c
Graig Patrick <graig.patrick@gmail.com>
2019-06-27 15:16:06 +0800

    updated rewrok file

--- lumen-framework/testfiles/TestFile13.php
+++ lumen-framework/testfiles/TestFile13.php
@@ -7,5 +7,11 @@ class TestFile13
     /**
      * TestFile13 constructor.
      */
-    public function __construct() { }
+    public function __construct() {
+        //Added new line here for the rework
+        //Added new line here for the rework
+        //Added new line here for the rework
+        //Added new line here for the rework
+        //Added new line here for the rework
+    }
 }
