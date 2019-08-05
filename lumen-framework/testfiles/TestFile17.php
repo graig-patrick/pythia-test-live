4f126f67cf696decb388b09ddd85790e09d86baa
graig-patrick <graig.patrick@gmail.com>
2019-08-05 13:50:45 +0800

    updated file (#90)
    Fixes for the previous ticket
    
    - Updated test file 17
    
    Ticket: 1068273
    Reviewer: Graig Patrick

--- lumen-framework/testfiles/TestFile17.php
+++ lumen-framework/testfiles/TestFile17.php
@@ -3,9 +3,16 @@
 
 class TestFile17
 {
+    /**
+     * @var string
+     */
+    private $param;
 
     /**
      * TestFile17 constructor.
+     * @param string $param
      */
-    public function __construct() { }
+    public function __construct(string $param) {
+        $this->param = $param;
+    }
 }
