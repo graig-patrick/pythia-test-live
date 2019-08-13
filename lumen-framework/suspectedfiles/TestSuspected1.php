8b2fc51a5bc7ca0a54faaa5326473d2c3ccf2b18
graig-patrick <graig.patrick@gmail.com>
2019-08-13 17:49:45 +0800

    1068502 rework multiple commit (#96)
    Another rework ticket
    
    - Updated rework ticket
    
    Ticket: 1068502
    Reviewer: Graig Patrick

--- lumen-framework/suspectedfiles/TestSuspected1.php
+++ lumen-framework/suspectedfiles/TestSuspected1.php
@@ -11,9 +11,9 @@ class TestSuspected1
 
     public function getRandomNumber()
     {
-        $randomNumber = rand(0,3500);
-        $randomNumber += 105;
-        $randomNumber -= rand(0, 5);
+        $randomNumber = rand(150,2500);
+        $randomNumber += 50;
+        $randomNumber -= rand(0, 150);
         return $randomNumber;
     }
 }
