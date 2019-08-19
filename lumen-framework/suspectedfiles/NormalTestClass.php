07bb2b21af935cc08c127b82eda68ae1fd701c70
graig-patrick <graig.patrick@gmail.com>
2019-08-19 13:44:02 +0800

    1069295 bug fix (#101)
    Bug fixing class
    
    - bugfix
    - added fixes
    
    Ticket: 1069295
    Reviewer: Graig Patrick

--- lumen-framework/suspectedfiles/NormalTestClass.php
+++ lumen-framework/suspectedfiles/NormalTestClass.php
@@ -6,6 +6,9 @@ class NormalTestClass
 
     /**
      * NormalTestClass constructor.
+     * @param $number
      */
-    public function __construct() { }
+    public function __construct($number) {
+        $number += 11;
+    }
 }
